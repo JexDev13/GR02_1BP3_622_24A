dfc7a3ab5cd7f1c3ece2047507f4e113df8f63e9
JexDev13 <jeremy.arias@epn.edu.ec>
2024-05-18 03:16:47 -0500

    CI Prueba 5

--- src/main/java/ec/edu/epn/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/CalculatorExecute.java
@@ -26,6 +26,10 @@ public class CalculatorExecute {
         System.out.println("ESTE ES UN CAMBIO 2 EL 19-05-2024 - MANUAL");
         System.out.println("ESTE ES UN CAMBIO 3 EL 19-05-2024 - MANUAL");
         System.out.println("ESTE ES UN CAMBIO 4 EL 19-05-2024 - MANUAL");
+        System.out.println("ESTE ES UN CAMBIO 5 EL 19-05-2024 - MANUAL");
+
+
+
         /*Scanner scanner = new Scanner(System.in);
 
         boolean continuar = true;
