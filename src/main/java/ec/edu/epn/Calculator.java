dc15120c9ba9253a7801df8a2ba72f3297a56583
JodualD <jorge.duenas@epn.edu.ec>
2024-05-18 21:11:25 -0500

    Suma Extra

--- src/main/java/ec/edu/epn/Calculator.java
+++ src/main/java/ec/edu/epn/Calculator.java
@@ -15,6 +15,10 @@ public class Calculator {
         return a * b;
     }
 
+    public int sumaExtra(int c, int d) {
+        return 5;
+    }
+
     public double divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
